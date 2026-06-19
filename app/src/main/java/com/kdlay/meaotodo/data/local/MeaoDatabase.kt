28b8f7a41db442b1becabc4779a7baf8e723f638
KDLay1 <3393426904@qq.com>
2026-06-19 19:35:13 +0800

    feat(todo): register task lists table

--- app/src/main/java/com/kdlay/meaotodo/data/local/MeaoDatabase.kt
+++ app/src/main/java/com/kdlay/meaotodo/data/local/MeaoDatabase.kt
@@ -6,23 +6,27 @@ import com.kdlay.meaotodo.data.local.dao.LedgerDao
 import com.kdlay.meaotodo.data.local.dao.PomodoroDao
 import com.kdlay.meaotodo.data.local.dao.SyncOutboxDao
 import com.kdlay.meaotodo.data.local.dao.TaskDao
+import com.kdlay.meaotodo.data.local.dao.TaskListDao
 import com.kdlay.meaotodo.data.local.entity.LedgerEntryEntity
 import com.kdlay.meaotodo.data.local.entity.PomodoroSessionEntity
 import com.kdlay.meaotodo.data.local.entity.SyncOutboxEntity
 import com.kdlay.meaotodo.data.local.entity.TaskEntity
+import com.kdlay.meaotodo.data.local.entity.TaskListEntity
 
 @Database(
     entities = [
         TaskEntity::class,
+        TaskListEntity::class,
         PomodoroSessionEntity::class,
         LedgerEntryEntity::class,
         SyncOutboxEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = true
 )
 abstract class MeaoDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
+    abstract fun taskListDao(): TaskListDao
     abstract fun pomodoroDao(): PomodoroDao
     abstract fun ledgerDao(): LedgerDao
     abstract fun syncOutboxDao(): SyncOutboxDao
