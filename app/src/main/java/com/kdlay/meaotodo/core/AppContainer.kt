0bff90884d5cad9568e4cf967718d0d35d6e0790
KDLay1 <3393426904@qq.com>
2026-06-19 17:10:31 +0800

    feat: add app dependency container

---
+++ app/src/main/java/com/kdlay/meaotodo/core/AppContainer.kt
@@ -0,0 +1,27 @@
+package com.kdlay.meaotodo.core
+
+import android.content.Context
+import androidx.room.Room
+import com.kdlay.meaotodo.data.local.MeaoDatabase
+import com.kdlay.meaotodo.data.repository.LedgerRepository
+import com.kdlay.meaotodo.data.repository.PomodoroRepository
+import com.kdlay.meaotodo.data.repository.TaskRepository
+import com.kdlay.meaotodo.sync.AndroidNsdWifiDiscoveryService
+import com.kdlay.meaotodo.sync.WifiSyncGateway
+
+class AppContainer(context: Context) {
+    private val appContext = context.applicationContext
+
+    val database: MeaoDatabase = Room.databaseBuilder(
+        appContext,
+        MeaoDatabase::class.java,
+        "meao_todo.db"
+    ).build()
+
+    val taskRepository = TaskRepository(database.taskDao(), database.syncOutboxDao())
+    val pomodoroRepository = PomodoroRepository(database.pomodoroDao(), database.syncOutboxDao())
+    val ledgerRepository = LedgerRepository(database.ledgerDao(), database.syncOutboxDao())
+
+    val wifiDiscoveryService = AndroidNsdWifiDiscoveryService(appContext)
+    val wifiSyncGateway = WifiSyncGateway(database.syncOutboxDao())
+}
